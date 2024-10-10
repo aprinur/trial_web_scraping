664ca4c26600821dc66efebcc2867f215496cbc5
Apri Nur <aprinur7@gmail.com>
2024-10-10 21:11:56 +0700

    scraping List of largest companies in Indonesia

--- wikipedia/main.py
+++ wikipedia/main.py
@@ -1,11 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas
+import datetime
 
+# getting url requests
 url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_Indonesia'
-
 page = requests.get(url)
-
 soup = BeautifulSoup(page.text, 'html.parser')
 
-table = soup.find_all('th', class_='headerSort')
-print(table)
+# selecting the first table
+table = soup.find_all('table', class_='wikitable sortable')[0]
+
+# getting table head
+table_head = [head.text.strip() for head in table.find_all('th')]
+df = pandas.DataFrame(columns=table_head)
+
+# getting table content
+rows = table.find_all('tr')
+for row in rows[1:]:
+    row_data = row.find_all('td')
+    item = [cell.text.strip() for cell in row_data]
+    # print(item)
+    length = len(df)
+    df.loc[length + 1] = item
+
+print(df)
+
+today = datetime.datetime.now().strftime('%d-%B-%Y')
+df.to_excel(f'List_of_largest_companies_in_Indonesia_{today}.xlsx', index=False)
