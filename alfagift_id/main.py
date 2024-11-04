635e90e871f1db9d1048c4c25d43254164c46617
Apri Nur <aprinur7@gmail.com>
2024-11-04 08:47:24 +0700

    done

--- alfagift_id/main.py
+++ alfagift_id/main.py
@@ -1,17 +1,16 @@
-from source.util import get_html, press_button, get_current_url, wait_emergence, save_to_file
+from source.util import get_html, press_button, get_current_url, save_to_file
 from source.soup import Soup
 import time
 from source.__init__ import URL
 
 
 def get_item():
-    url = 'https://alfagift.id/p/disney-lotso-tempat-minum--811091'
+    url = 'https://alfagift.id/p/alfamart-sabun-cuci-piring-lime-650-ml-56255'
     html = get_html(url)
     soup = Soup(html)
-    output = []
     time.sleep(3)
-    soup.scrape_product()
-    print(output)
+    result = soup.scrape_product()
+    print(result)
 
 
 def get_item_urls():
@@ -79,51 +78,40 @@ def main():
     category_urls = soup.scrape_category_url()   # Scrape category urls from main page
     result = []
 
-    # Loop through each category URL
-    for category_url in category_urls[:3]:
-        print(f'Opening category: {category_url}')
-        html = get_html(category_url)
-        product_page = Soup(html)
-
-        # Start scraping product URLs from category page
-        while True:
-            # Scrape product URLs from current page
-            product_urls = product_page.scrape_product_url()
+    # list to contain product url
+    product_urls = []
+    while True:
+        # Loop through each category URL
+        for category_url in category_urls[:3]:
+            print(f'Opening category: {category_url}')
+            html = get_html(category_url)
+            product_page = Soup(html)
+            product_urls.append(product_page.scrape_product_url())
             while True:
+                press_button('//button[@role="menuitem" and @aria-label="Go to next page" and @class="page-link"]')
+                time.sleep(5)
+                product_page = Soup(get_current_url())
+                product_urls.append(product_page.scrape_product_url())
+                print(product_urls)
+                # if product_page.final_page():
+                #     break
+                if product_page.scrape_active_page() == '2':
+                    break
+        # loop url in product_urls
+        product_urls = [element for innerlist in product_urls for element in innerlist]
+        for product_url in product_urls:
+            print(product_url)
+            html = get_html(product_url)
+            soup = Soup(html)
+            time.sleep(5)
+            result.append(soup.scrape_product())
 
-                for product_url in product_urls[:3]:
-                    print(f'Scraping product: {product_url}')
-                    html = get_html(product_url)
-                    time.sleep(2)
-                    soup = Soup(html)
-
-                    # Scrape product information and store in result
-                    product = soup.scrape_product()
-                    result.append(product)
-
-                # Check if already in the last page
-                total_pages = product_page.check_total_page()
-                current_page = product_page.scrape_active_page()
-
-                if current_page == total_pages:
-                    print(f'Reached the last page of category {category_url}')
-                    break  # Exit loop after reaching the last page
-                else:
-                    # move to the next page in the category if not in the last page
-                    page = soup.previous_page()
-                    html = get_html(page)
-                    press_button('//button[@role="menuitem" and @aria-label="Go to next page" and @class="page-link"]')
-                    time.sleep(3)
-                    product_page = Soup(html)
-                    product_urls = product_page.scrape_product_url()
-
-    save_to_file(result, 'Trial')  # Save the result
+        save_to_file(result, 'Product_from_first_3_category_and_first_2_page')
+        break
 
 
 if __name__ == '__main__':
     main()
 
 
-"""
-Pr: masih belum bisa scrape produk dihalaman ke 2 dst 
-"""
+
--- alfagift_id/source/soup.py
+++ alfagift_id/source/soup.py
@@ -10,19 +10,18 @@ class Soup(BeautifulSoup):
     def scrape_product(self):
         """Method to scrape information from the product"""
         table = self.find('div', class_='col-sm-12 col-md-12 col-lg-5 col-12')
-        tittle = (table.find('p', class_='text-xlg fw7 d-none d-sm-none d-md-none d-lg-block')
-                  .get_text().strip())
-        brand = table.find('a', class_='text-sm fw7 text-secondary text-decoration-none ml-2').get_text().capitalize()
-
+        tittle = table.find('p', class_='text-xl fw5 d-lg-none').get_text().strip()
+        if table.find('a', class_='text-sm fw7 text-secondary text-decoration-none ml-2'):
+            brand = table.find('a', class_='text-sm fw7 text-secondary text-decoration-none ml-2').get_text().capitalize()
+        else:
+            brand = None
         original_price = None
         if table.find('del', class_='text-neutral-60 text-sm'):
             original_price = table.find('del', class_='text-neutral-60 text-sm').get_text().strip()
         elif table.find('p', class_='text-xlg fw7 text-primary mb-1'):
             original_price = table.find('p', class_='text-xlg fw7 text-primary mb-1').get_text().strip()
         elif table.find('div', class_='alert alert-danger text-sm border-0 rounded-md'):
-            original_price = (table.find('div',
-                                         class_='alert alert-danger text-sm border-0 rounded-md')
-                              .get_text().strip())
+            original_price = (table.find('div', class_='alert alert-danger text-sm border-0 rounded-md').get_text().strip())
 
         discount = table.find('span', class_='badge badge-disc mr-1').get_text() if (
             table.find('span', class_='badge badge-disc mr-1')) else 'None'
@@ -44,6 +43,7 @@ class Soup(BeautifulSoup):
 
         description = [d.get_text() for d in self.find('div', class_='description-box').find_all('li')]
 
+        # print(brand)
         return Items(
             Tittle=tittle,
             Brand=brand,
